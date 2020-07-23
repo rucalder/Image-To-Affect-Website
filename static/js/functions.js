dc3c2330ccedbe369798a6f39c757982cb665763
loveshackty <52482810+loveshackty@users.noreply.github.com>
2020-07-22 21:34:00 -0700

    pre-cleaning push (for sanity if things go wrong)

--- static/js/functions.js
+++ static/js/functions.js
@@ -2,10 +2,12 @@
 csvPath = "../data/image_path.csv"
 vectorPath = "../data/liwc_toParse.csv"
 //var allFilePaths = []
+var clicks = 0;
 var score = 0;
 var questions = 0;
 var ranNum = 0;
 var currVect = [];
+var emoteTypes = ['anger', 'anxiety', 'positivity', 'sadness', 'love'];
 
 function readCSVToArray(path, delimter) {
     var filePaths = []
@@ -45,11 +47,11 @@ function show_image() {
     //clear the "resultDiv"
     document.getElementById("resultDiv").innerHTML = "";
 
-    allFilePaths = readCSVToArray(csvPath, '\r')
-    allVectorPaths = readCSVToArray(vectorPath, '\r')
+    allFilePaths = readCSVToArray(csvPath, '\r');
+    allVectorPaths = readCSVToArray(vectorPath, '\r');
 
     ranNum = Math.floor(Math.random() * allFilePaths.length);
-    data = allVectorPaths[ranNum].split(",")
+    data = (allVectorPaths[ranNum]).split(",");
     currVect = data.slice(1,6);
 
     var img = document.createElement("img");
@@ -57,7 +59,7 @@ function show_image() {
     img.id = "picture";
     img.height = "600";
 
-    console.log(allFilePaths[ranNum])
+    console.log(allFilePaths[ranNum]);
 
     var foo = document.getElementById("resultDiv");
     foo.appendChild(img);
@@ -93,29 +95,42 @@ function saveAffects() {
 //stored, along with the affect vector for the image, and the path to the image. After 5 images, all info
 // should be shown to user
 
-function onButtonClicked() {
+function onButtonClicked(clicked_id) {
     //have to create counter to keep track of button clicks, change when button has been
     //clicked 5 times
-    paintingVector = getVector();
-    pred = saveAffects();
-    questions++;
-    if (pred == 'anger'){
+    //paintingVector = getVector();
+    //pred = saveAffects();
+
+    // disable button so it can't be clicked again
+    document.getElementById(clicked_id).disabled = true;
+
+    // Get clicked button emotion
+    clicked_emotion = clicked_id.split("_")[0];
+    // compare against expected (CHANGE CHECK TO BE REAL DATA)
+    if(clicked_emotion == emoteTypes[Math.floor(Math.random() * emoteTypes.length)]){
         score++;
     }
-    $('input[name=emotion]').attr('checked',false);
-    show_image();
-    if(questions >= 5){
-        document.getElementById("submitButton").textContent = score.toString();
-        // GO TO END SCREEN (SCORE SCREEN)
+    clicks++;
+    // reset page if buttons have been completed
+    if(clicks >= emoteTypes.length){
+        clicks = 0;
+        questions++;
+        show_image();
+        for(var i = 0; i < emoteTypes.length; i++){
+            document.getElementById(emoteTypes[i] + '_button').disabled = false;
+        }
+        if(questions >= 5){
+            sessionStorage.setItem('playerScore', score.toString);
+            window.location.href = "/static/html/user_input.html";
+            // GO TO END SCREEN (SCORE SCREEN)
+        }
     }
-    //return pred
+    
 }
 
 
 $(document).ready(function () {
-
     show_image()
-
 })
     // vector = getVector()
 
